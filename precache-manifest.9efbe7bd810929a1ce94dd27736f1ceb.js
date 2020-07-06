68ed2e04add7cea3ee1a5e3e64a7bef659e8083c
SergeyKolishkin <Sparxxx1986@gmail.com>
2020-07-06 16:57:17 +0300

    Updates

---
+++ precache-manifest.9efbe7bd810929a1ce94dd27736f1ceb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d5bf726c5b2fcfa514c8b94397d653b",
+    "url": "/DemoSocialNetwork/index.html"
+  },
+  {
+    "revision": "2cbd1b35fbbc74aea303",
+    "url": "/DemoSocialNetwork/static/css/main.6aa4ffdf.chunk.css"
+  },
+  {
+    "revision": "0f91bebb3b70a2f2f360",
+    "url": "/DemoSocialNetwork/static/js/2.4a3e8f83.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/DemoSocialNetwork/static/js/2.4a3e8f83.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cbd1b35fbbc74aea303",
+    "url": "/DemoSocialNetwork/static/js/main.e85dd5e3.chunk.js"
+  },
+  {
+    "revision": "b74faa6cd85622daaa34",
+    "url": "/DemoSocialNetwork/static/js/runtime-main.e308061e.js"
+  },
+  {
+    "revision": "3b00bc5733438082ab8be80877411cd8",
+    "url": "/DemoSocialNetwork/static/media/loader.3b00bc57.gif"
+  }
+]);
